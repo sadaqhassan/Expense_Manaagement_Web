21f64af42d80d66ef49e42aef3e7837b1640aaac
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-04 15:31:16 +0300

    expenses

---
+++ client/src/Pages/Expenses.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Table from '../Components/Table'
+
+const Expenses = () => {
+  return (
+    <div>
+        <h1 className='text-xl text-gray-900 font-medium'>Expense Management </h1>
+        <p className='text-sm text-gray-600 mt-1'>Feel Free to Manage Your expense Add , update , and delete</p>
+
+        <div className='mt-16'>
+            <Table/>
+        </div>
+    </div>
+  )
+}
+
+export default Expenses
