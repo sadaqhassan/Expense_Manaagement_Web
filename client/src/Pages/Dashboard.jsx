533d4cd8e26d17135f0679b2967957baa95963f2
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-04 17:51:05 +0300

    dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BanknoteArrowDown, CalendarCheck2Icon, DollarSignIcon } from 'lucide-react'
+import Table from '../Components/Table'
+import { useAppContext } from '../Context/AppContext'
 
 const Dashboard = () => {
+    const { dashboard } = useAppContext()
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col justify-between sticky '>
+    <div className='grid grid-cols-2 md:grid-cols-4 gap-6 top-10'>
+        {
+            dashboard.map((data)=>(
+                <div style={{backgroundColor:data.color}} className={`shadow-xl rounded text-white w-46 h-20`}>  
+                <div className='flex justify-between px-2 py-2'>
+                    <p className='text-sm '>Last {data.Short}</p>
+                    <BanknoteArrowDown/>
+                </div>
+                <p className='text-center mb-3 font-medium'>{data.total}</p>
+                </div>
+            ))
+        }
+    </div>
+    </div>
   )
 }
 
