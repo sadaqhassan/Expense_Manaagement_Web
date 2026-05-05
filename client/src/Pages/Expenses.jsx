3dcbcbf2acafdad8a3789dd1685471900c223cd4
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-05 14:17:37 +0300

    expnse ui

--- client/src/Pages/Expenses.jsx
+++ client/src/Pages/Expenses.jsx
@@ -7,7 +7,7 @@ const Expenses = () => {
         <h1 className='text-xl text-gray-900 font-medium'>Expense Management </h1>
         <p className='text-sm text-gray-600 mt-1'>Feel Free to Manage Your expense Add , update , and delete</p>
 
-        <div className='mt-16'>
+        <div className='mt-6'>
             <Table/>
         </div>
     </div>
