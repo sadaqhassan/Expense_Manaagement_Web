9c16b501b93dafaaa9572884ecdfe6fc7bd12fc8
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-06 19:47:06 +0300

    usercontroller

--- server/Controllers/user.controller.js
+++ server/Controllers/user.controller.js
@@ -86,3 +86,21 @@ export const deleteUser = async (req, res) => {
         console.log("Error deleting user:", error);
     }
 }
+
+
+//getUSer 
+
+export const getUser = async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        const user = await User.findById(req.userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        res.status(200).json({ success: true, userData: user });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error" + error.message });
+        console.log("Error fetching user:", error);
+    }
+}
