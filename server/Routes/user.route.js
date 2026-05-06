4ec0c6fa0e46ae84cb51bd0cd662c243826df6da
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-06 15:20:09 +0300

    user api

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUsers, loginUser, registerUser } from "../Controllers/user.controller.js";
+import { deleteUser, getAllUsers, loginUser, registerUser } from "../Controllers/user.controller.js";
 import { isAdmin, isAuth } from "../Middlewares/isAdmin.js";
 
 const user = Router()
@@ -7,5 +7,6 @@ const user = Router()
 user.post("/register", registerUser);
 user.post("/login", loginUser);
 user.get("/get-users",isAuth,isAdmin,getAllUsers);
+user.delete("/delete-user/:id",isAuth,isAdmin,deleteUser);
 
 export const userRoute = user;
