866007e4e973ab8b9ae5e3755c9e4ea4ede976fc
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-07 11:13:35 +0300

    expeense route

--- server/Routes/expenses.route.js
+++ server/Routes/expenses.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addExpense } from "../Controllers/expenses.controller.js";
+import { addExpense, deleteExpense, getExpenses } from "../Controllers/expenses.controller.js";
 import { isAuth } from "../Middlewares/isAdmin.js";
 
 const expense = Router();
 
 expense.post("/add-expense",isAuth, addExpense);
+expense.get("/get-expenses",isAuth, getExpenses);
+expense.delete("/delete-expense/:id",isAuth, deleteExpense);
 
 export const expenseRoute = expense;
