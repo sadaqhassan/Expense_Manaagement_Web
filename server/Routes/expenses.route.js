50931f0b4e31a8dbc05a9a96e78175ee8d147e50
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-07 11:54:41 +0300

    expense route

--- server/Routes/expenses.route.js
+++ server/Routes/expenses.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { addExpense, deleteExpense, getExpenses } from "../Controllers/expenses.controller.js";
-import { isAuth } from "../Middlewares/isAdmin.js";
+import { addExpense, deleteExpense, getAllExpenses, getExpenses } from "../Controllers/expenses.controller.js";
+import { isAdmin, isAuth } from "../Middlewares/isAdmin.js";
 
 const expense = Router();
 
 expense.post("/add-expense",isAuth, addExpense);
 expense.get("/get-expenses",isAuth, getExpenses);
 expense.delete("/delete-expense/:id",isAuth, deleteExpense);
+expense.get("/get-all-expenses",isAuth,isAdmin, getAllExpenses);
 
 export const expenseRoute = expense;
