9a5ba4247a59e6c86345d0903854f566490a3635
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-07 11:13:42 +0300

    user route

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteUser, getAllUsers, getUser, loginUser, registerUser } from "../Controllers/user.controller.js";
+import { deleteUser, getAllUsers, getUser, loginUser, logoutUser, registerUser } from "../Controllers/user.controller.js";
 import { isAdmin, isAuth } from "../Middlewares/isAdmin.js";
 
 const user = Router()
@@ -9,5 +9,6 @@ user.post("/login", loginUser);
 user.get("/get-users",isAuth,isAdmin,getAllUsers);
 user.delete("/delete-user/:id",isAuth,isAdmin,deleteUser);
 user.get("/get-user",isAuth,getUser);
+user.get("/logout",isAuth,logoutUser);
 
 export const userRoute = user;
